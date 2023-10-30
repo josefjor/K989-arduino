2278c5087be579e7ab663631e02ce9e29019bed7
Josef Jörgensen <josef.jorgensen@hotmail.se>
2023-10-30 19:21:00 +0100

    Completed motor control

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Warning!
-This is not yet tested. Use at your own risk!
-
 # K989-arduino
 Base for controlling the WLtoys K989 using an arduino instead of included transmitter and receiver
 
@@ -9,4 +6,4 @@ This project is currently in early development, and the code is not complete yet
 
 In addition to the K989 car you will need the following:
 - Arduino Uno
-- Speed controller (ESC)
+- L298N Dual H-bridge
--- main/main.ino
+++ main/main.ino
@@ -17,8 +17,11 @@ int currentSteering;
 
 // Define variables
 const byte motorPin = 6;
-Servo motor; // A ESC for R/C uses the same signals as servos.
-const byte maxSpeed = 135; // Set half of motor capacity as max power.
+const byte minSpeed=100;
+const byte maxSpeed = 150;
+
+const byte directionPin1 = 4;
+const byte directionPin2 = 5;
 
 int currentSpeed;
 
@@ -31,32 +34,68 @@ void setup() {
 
   // Motor setup
   pinMode(motorPin, OUTPUT);
-  motor.attach(motorPin);
   currentSpeed=0;
   setSpeed(currentSpeed);
-  
+
+  pinMode(directionPin1, OUTPUT);
+  pinMode(directionPin2, OUTPUT);
+
 
   // Start serial communication
   Serial.begin(9600);
 }
 
 void loop() {
-  
+  // Speed up motor then slow down again
+
+  for (currentSpeed = 0; currentSpeed < 100; currentSpeed++) {
+    setSpeed(currentSpeed);
+    delay(100);
+  }
+  for (currentSpeed = 100; currentSpeed > 0; currentSpeed--) {
+    setSpeed(currentSpeed);
+    delay(100);
+  }
+
+
+  for (currentSpeed = 0; currentSpeed >-100; currentSpeed--) {
+    setSpeed(currentSpeed);
+    delay(100);
+  }
+  for (currentSpeed = -100; currentSpeed < 0; currentSpeed++) {
+    setSpeed(currentSpeed);
+    delay(100);
+  }
+
 }
 
 void setSpeed(int speed) {
   /* Params: speed: int between -100 and 100 where -100 is maximum backwards 
-    and 100 is maximum forwards¨  
+    and 100 is maximum forwards 
   */ 
   // Cancel the action if trying to steer more than allowed
   if (abs(speed) > 100) {
     return;
   }
+
+  // Set direction
+  if (speed > 0) {
+    digitalWrite(directionPin2, LOW);
+    digitalWrite(directionPin1, HIGH);
+  } else if (speed < 0) {
+    digitalWrite(directionPin1, LOW);
+    digitalWrite(directionPin2, HIGH);
+  } else {
+    digitalWrite(directionPin1, LOW);
+    digitalWrite(directionPin2, LOW);
+  }
+
   // Map the value into servo degrees
-  const int motorPower = map(speed, -100, 100, 180-maxSpeed, maxSpeed);
-  motor.write(motorPower);
+  const int motorPower = map(abs(speed), 0, 100, minSpeed, maxSpeed);
+  analogWrite(motorPin, motorPower);
   return;
 }
+
 
 void setSteering(int steering) {
   /* Params: steering: int between -100 and 100 where -100 is maximum turning to left 
